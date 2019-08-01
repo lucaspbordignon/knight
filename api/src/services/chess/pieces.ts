40cdd038516c172647edef15659a3f4277d20568
Lucas Bordignon <bordi.lucas@gmail.com>
2019-08-01 15:20:13 -0300

    Update pieces service to find possible moves for a Knight

--- api/src/services/chess/pieces.ts
+++ api/src/services/chess/pieces.ts
@@ -1,14 +1,51 @@
-import _ from 'lodash'
-
 import { PositionChecker } from './utils'
 
 const BOARD_SIZE = 8
 
 /* Apply Breadth First Search to find all positions */
 const possibleMoves = (position: string, turns: number, boardSize: number = BOARD_SIZE): Array<Number> => {
-  const bitmapPosition = new PositionChecker(position).toBitmap(boardSize)
+  const checker = new PositionChecker(position)
+  const bitmapPosition = checker.toBitmap(boardSize)
 
-  return [bitmapPosition]
+  if (checker.algebraicPosition()) return allValidMoves(0, turns, checker.toBitmap(boardSize))
+
+  return allValidMoves(0, turns, parseInt(position))
+}
+
+/* Find all valid moves based on an initial position and number of turns */
+const allValidMoves = (
+  currentTurn: number,
+  finalTurn: number,
+  initialPosition?: number,
+  computedMoves: object = {},
+): Array<number> => {
+  if (currentTurn === 0) computedMoves[currentTurn.toString()] = new Set([initialPosition])
+
+  const nextTurn = currentTurn + 1
+  const reachablePositions = [...computedMoves[currentTurn.toString()]]
+
+  computedMoves[nextTurn.toString()] = new Set()
+
+  reachablePositions.map((position) => validMoves(position).map((move) => computedMoves[nextTurn].add(move)))
+
+  if (nextTurn === finalTurn) {
+    return [...computedMoves[finalTurn]]
+  }
+
+  return allValidMoves(nextTurn, finalTurn, initialPosition, computedMoves)
+}
+
+/* All valid moves for the given position */
+const validMoves = (position: number): Array<number> => {
+  return PositionChecker.displacements
+    .map((delta) => {
+      const finalPos = delta + position
+
+      if (PositionChecker.validMove(finalPos)) {
+        return finalPos
+      }
+    })
+    .filter((pos) => pos)
 }
 
 export { possibleMoves }
--- api/src/services/chess/utils.ts
+++ api/src/services/chess/utils.ts
@@ -1,19 +1,31 @@
 class PositionChecker {
   static readonly columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
   static readonly rows = Array.from({ length: 8 }, (_, i) => i + 1)
+  static readonly displacements = [-17, -15, -10, -6, 6, 10, 15, 17]
 
   errors: string
+
+  static validMove(position: number): boolean {
+    if (position > 0 && position < 64) {
+      return true
+    }
+
+    return false
+  }
+
+  /* Instance methods */
 
   constructor(public position: string) {
     this.position = position
     this.errors = null
   }
 
-  valid(): boolean {
+  public valid(): boolean {
     return this.algebraicPosition()
   }
 
-  algebraicPosition(): boolean {
+  /* Check whether the given position is algebraic */
+  public algebraicPosition(): boolean {
     const validationRegex = /^([A-Z]+)([0-9]+)$/
 
     if (validationRegex.test(this.position)) {
@@ -25,7 +37,18 @@ class PositionChecker {
     return false
   }
 
-  getCol(): string {
+  /* Converts Algebraic coordinates to Bitmap coordinates [1-64] */
+  public toBitmap(boardSize: number): number {
+    const multiplier = PositionChecker.columns.indexOf(this.getCol())
+
+    if (multiplier < 0) {
+      return null
+    }
+
+    return multiplier * boardSize + this.getRow()
+  }
+
+  private getCol(): string {
     const colRegex = /^([A-Z]+)/
 
     if (!this.valid()) {
@@ -35,7 +58,7 @@ class PositionChecker {
     return colRegex.exec(this.position)[0]
   }
 
-  getRow(): number {
+  private getRow(): number {
     const rowRegex = /([0-9]+)$/
 
     if (!this.valid()) {
@@ -43,16 +66,6 @@ class PositionChecker {
     }
 
     return parseInt(rowRegex.exec(this.position)[0])
-  }
-
-  toBitmap(boardSize: number): number {
-    const multiplier = PositionChecker.columns.indexOf(this.getCol())
-
-    if (multiplier < 0) {
-      return null
-    }
-
-    return multiplier * boardSize + this.getRow()
   }
 }
 
