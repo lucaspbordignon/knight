7cf51291787aa3359202707dd3fca101840968aa
Lucas Bordignon <bordi.lucas@gmail.com>
2019-08-02 11:29:40 -0300

    Update chess ducks to add moves fetcher

--- client/src/redux/ducks/chess.ts
+++ client/src/redux/ducks/chess.ts
@@ -2,14 +2,32 @@ import { handleActions } from 'redux-actions'
 import { createActionThunk } from 'redux-thunk-actions'
 
 import { getBoardData } from '../../api/chess/board'
+import { getPossibleMovesData } from '../../api/chess/pieces'
+
+/* Types */
+const Types = {
+  BOARD: 'chess/GET_BOARD',
+  MOVES: 'chess/GET_POSSIBLE_MOVES',
+}
+
+/* Actions */
+export const getBoard = createActionThunk(Types.BOARD, () => getBoardData())
+
+export const getPossibleMoves = createActionThunk(Types.MOVES, ({ position, turns }) =>
+  getPossibleMovesData(position, turns),
+)
+
+/* Reducers */
 
 export const initialState = {
   board: [],
+  boardSize: 8,
+  currentPosition: null,
+  possibleMoves: [],
+
   loading: false,
   error: null,
 }
-
-export const getBoard = createActionThunk('GET_BOARD', () => getBoardData())
 
 export const chess = handleActions(
   {
@@ -30,6 +48,20 @@ export const chess = handleActions(
     [getBoard.ENDED]: (state) => ({
       ...state,
       loading: false,
+    }),
+
+    [getPossibleMoves.STARTED]: (state, action) => ({
+      ...state,
+      currentPosition: action.payload.position,
+    }),
+    [getPossibleMoves.SUCCEEDED]: (state, action) => ({
+      ...state,
+      possibleMoves: action.payload.data.possibleMoves,
+      error: null,
+    }),
+    [getPossibleMoves.FAILED]: (state, action) => ({
+      ...state,
+      error: action.payload.error,
     }),
   },
   initialState,
